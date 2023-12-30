5f9177ce2fd9d5cbbc84a3ded28a61e8d8b88eb6
codeGeek16 <sahil.aieee@gmail.com>
2023-12-30 10:31:09 +0530

    optimized solution as we can start from 2 since a number is always divisible by itself and 1

--- src/com/sahil/loops/Lab14.java
+++ src/com/sahil/loops/Lab14.java
@@ -9,15 +9,14 @@ public class Lab14 {
 
     public static void main(String[] args) {
         int n = 16;
-        int count =0;
-        for (int i = 1; i <= n/2; i++) { //as all multiples will be covered till this range
+        int count =2;
+        for (int i = 2; i <= n/2; i++) { //as all multiples will be covered till this range
             if (n % i == 0)
                 count ++;
         }
-        count ++;
         System.out.println(count); // as number is divisible by itself also, and it won't cover in above loop
     }
 }
 
 //time complex = O(n/2)
-//space complexity=O(1)
+//space complexity=O(n/2)
