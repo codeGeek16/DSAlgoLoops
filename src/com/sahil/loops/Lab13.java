73ca9d3f3a5d3513494ef36e14f88a9e18eda078
codeGeek16 <sahil.aieee@gmail.com>
2023-12-30 10:01:52 +0530

    initial pass print divisibles of a number

--- src/com/sahil/loops/Lab13.java
+++ src/com/sahil/loops/Lab13.java
@@ -9,13 +9,13 @@ public class Lab13 {
 
     public static void main(String[] args) {
         int n = 16;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n/2; i++) { //as all multiples will be covered till this range
             if (n % i == 0)
                 System.out.println(i);
         }
-
+        System.out.println(n); // as number is divisible by itself also, and it won't cover in above loop
     }
 }
 
-//time complex = O(n)
+//time complex = O(n/2)
 //space complexity=O(1)
